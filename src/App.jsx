532cc8774676cc389ad4547f27693b674c6e7700
Athira Saji <athira1355@gmail.com>
2026-03-20 17:49:26 +0530

    props-multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ function App() {
     return(
         <>
             <Student name="Alex" age={20} isStudent={true}/>
+            <Student name="Taylor" age={41} isStudent={false}/>
+            <Student name="Sara" age={50} isStudent={false}/>
+            <Student name="Alice" age={23} isStudent={true}/>
         </>
     );
 }
